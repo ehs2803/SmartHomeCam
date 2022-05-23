375d82449e8442e64ebd8b18a239fdcf21ba50e0
ehs2803 <hr2803@naver.com>
2022-05-23 09:40:06 +0900

    song-20220523

---
+++ Raspberry Pi/songtest.py
@@ -0,0 +1,5 @@
+
+import pygame #pip install pygame
+pygame.mixer.init()# initializing the module
+pygame.mixer.music.load('sleepSong.mp3')# loading the ogg file
+pygame.mixer.music.play()# this will simply play the loaded file.
