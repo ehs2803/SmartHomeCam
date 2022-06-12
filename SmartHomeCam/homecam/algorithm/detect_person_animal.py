3cd628e265d46cd2a172b657c5cff1e7c45a1291
ehs2803 <hr2803@naver.com>
2022-06-12 09:55:52 +0900

    safemode_noaction-20220612

--- SmartHomeCam/homecam/algorithm/detect_person_animal.py
+++ SmartHomeCam/homecam/algorithm/detect_person_animal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c54c8d7d502179e5783845f5a7be0c4dc5d36e602d60bae88450e602250fd79a
+oid sha256:5f2cbed750dec545efb13bc51b0b59629fff64a6dbf60f395b7e05a69139e56f
 size 9946
--- SmartHomeCam/homecam/algorithm/safeMode.py
+++ SmartHomeCam/homecam/algorithm/safeMode.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:070ac73f7593c1b39c3319c7057383af6a7f874525cf90742114f2f762857c74
-size 4096
+oid sha256:ccae2d69e0eeda4d988aa47824f6282a54e97a5e59d520f942cc01c240c0ca8e
+size 5886
