3b0d8d9c2cd09232165bf23d6c7d5711ff074e4d
ehs2803 <hr2803@naver.com>
2022-09-01 09:11:03 +0900

    feat : 탐지결과 표형식+detail 페이지 구현

--- SmartHomeCam/SmartHomeCam/settings.py
+++ SmartHomeCam/SmartHomeCam/settings.py
@@ -11,10 +11,26 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+# Keep secret keys in secrets.json
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -154,9 +170,9 @@ WSGI_APPLICATION = 'SmartHomeCam.wsgi.application'
 DATABASES = {
  	'default': {
      'ENGINE': 'django.db.backends.mysql',
-     'NAME': 'smarthomecam',
-     'USER': 'root',
-     'PASSWORD': '1234',
+     'NAME':  get_secret("DB_NAME"),
+     'USER': get_secret("DB_USER"),
+     'PASSWORD': get_secret("DB_PASSWORD"),
      'HOST': 'localhost',
      'PORT': '3306',
      }
